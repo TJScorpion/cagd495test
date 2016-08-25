9c0c63c0db95c342027bad7b47b282583e1a0115
TJScorpion <tjscorpion93@gmail.com>
2016-08-25 16:33:45 -0700

    I have scripts for PlayerHealth and enemy movement, but couldn't figure out why things weren't working before end of class.

---
+++ Assets/Scripts/Character/PlayerHealth.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerHealth : MonoBehaviour {
+
+    public int health = 5;
+
+	// Use this for initialization
+	void Awake () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (health == 0) {
+			Application.LoadLevel (Application.loadedLevel);
+		}
+	}
+
+	void OnCollision(Collision col){
+		if (col.gameObject.tag == "enemy") {
+			health--;
+		}
+	}
+}
---
+++ Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyScript : MonoBehaviour {
+
+	public GameObject target1;
+	public GameObject target2;
+	public float speed = .25f;
+	public Vector3 move;
+
+	// Use this for initialization
+	void Awake () {
+		target1 = GameObject.FindGameObjectWithTag ("target1");
+		target2 = GameObject.FindGameObjectWithTag ("target2");
+		move = Vector3.right;
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(move * speed);
+	}
+
+	void OnTriggerEnter(Collider other){
+		if (other.gameObject.tag == "target1") {
+			move = Vector3.left;
+		} else {
+			move = Vector3.right;
+		}
+	}
+}
